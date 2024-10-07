b0cb2750897d630101d04df72f141de5cc479045
Nautikk <helicalnau@gmail.com>
2024-10-08 01:46:36 +0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask
+import RPi.GPIO as GPIO
+import time
+
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BOARD)
+
+pin_fan = 13
+
+GPIO.setup(pin_fan, GPIO.OUT)
+current_state = "isOff"
+
+@app.route('/turn_on')
+def turn_on():
+    global current_state
+    if current_state == "isOff": 
+        GPIO.output(pin_fan, GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(pin_fan, GPIO.LOW)
+        current_state = "isOn"
+        return 'Fan turned on'
+    else:
+        return 'Fan is already on'
+
+@app.route('/turn_off')
+def turn_off():
+    global current_state
+    if current_state == "isOn":
+        GPIO.output(pin_fan, GPIO.HIGH) 
+        time.sleep(0.2)
+        GPIO.output(pin_fan, GPIO.LOW)
+        current_state = "isOff"
+        return 'Fan turned off'
+    else:
+        return 'Fan is already off'
+
+@app.route('/switch')
+def switch():
+    GPIO.output(pin_fan, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(pin_fan, GPIO.LOW)
+    return 'switching'
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        GPIO.cleanup()
